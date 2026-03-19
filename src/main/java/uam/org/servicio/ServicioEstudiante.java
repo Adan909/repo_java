7e967a653c9d5199fc52774abd6fdf20fb145022
Adan909 <jorgeot@uamv.edu.ni>
2026-03-19 14:33:57 -0600

    clase 19-3

--- src/main/java/uam/org/servicio/ServicioEstudiante.java
+++ src/main/java/uam/org/servicio/ServicioEstudiante.java
@@ -1,35 +1,17 @@
 package uam.org.servicio;
 
-import uam.org.config.Global;
 import uam.org.model.Estudiante;
 
 public class ServicioEstudiante {
-    public static Estudiante insertarEstudiante(Estudiante estudiante)
-    {
-        Global.listaEstudiante.add(estudiante);
-        return estudiante;
-    }
-    public static void listarEstudiantes()
-    {
-        for(Estudiante estudiante : Global.listaEstudiante){
-            System.out.println(estudiante);
-        }
+    private String calificacion;
+
+    public static Double calcularBeca(InterfaceEstrategiaBeca estrategia, Estudiante estudiante) {
+        return estrategia.calcularBeca(estudiante);
+
+
     }
 
-    public static void activarEstudiante(Estudiante estudiante)
-    {
-        for(int i = 0; i< Global.listaEstudiante.size(); i++){
-            Estudiante estudiante1 = Global.listaEstudiante.get(i);
-            if(estudiante1.equals(estudiante)){
-                estudiante1.setNombre(estudiante.getNombre());
-                estudiante1.setApellido(estudiante.getApellido());
-                estudiante1.setCif(estudiante.getCif());
-                estudiante1.setCorreo(estudiante.getCorreo());
-            }
-        }
-    }
-    public static void eliminarEstudiante(Estudiante estudiante)
-    {
-        Global.listaEstudiante.remove(estudiante);
-    }
 }
+
+
+
---
+++ src/main/java/uam/org/servicio/ServicioEstudianteDB.java
@@ -0,0 +1,35 @@
+package uam.org.servicio;
+
+import uam.org.config.Global;
+import uam.org.model.Estudiante;
+
+public class ServicioEstudiante {
+    public static Estudiante insertarEstudiante(Estudiante estudiante)
+    {
+        Global.listaEstudiante.add(estudiante);
+        return estudiante;
+    }
+    public static void listarEstudiantes()
+    {
+        for(Estudiante estudiante : Global.listaEstudiante){
+            System.out.println(estudiante);
+        }
+    }
+
+    public static void activarEstudiante(Estudiante estudiante)
+    {
+        for(int i = 0; i< Global.listaEstudiante.size(); i++){
+            Estudiante estudiante1 = Global.listaEstudiante.get(i);
+            if(estudiante1.equals(estudiante)){
+                estudiante1.setNombre(estudiante.getNombre());
+                estudiante1.setApellido(estudiante.getApellido());
+                estudiante1.setCif(estudiante.getCif());
+                estudiante1.setCorreo(estudiante.getCorreo());
+            }
+        }
+    }
+    public static void eliminarEstudiante(Estudiante estudiante)
+    {
+        Global.listaEstudiante.remove(estudiante);
+    }
+}
